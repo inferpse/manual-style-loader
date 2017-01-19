30a8970ab2f2dbaedb046e422a1a0e45a708af86
Sergey <inferpse@users.noreply.github.com>
2017-01-19 13:38:13 +0200

    added chunk loaded hook

--- add-styles.js
+++ add-styles.js
@@ -1,9 +1,17 @@
-var css = [];
+var css = [], addChunkCallback;
 
 module.exports = function(cssCode) {
-  css.push(cssCode);
+  var chunk = cssCode.toString();
+  css.push(chunk);
+  if (addChunkCallback) {
+    addChunkCallback(chunk);
+  }
 }
 
 module.exports.getData = function() {
   return css;
+}
+
+module.exports.onAddChunk = function(callback) {
+  addChunkCallback = callback;
 }
